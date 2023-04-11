75f58f7e6f75c6cd570715af5995b16685cc283c
georas94 <rashid.tamboura@icloud.com>
2023-04-11 07:22:57 +0200

    Update project slider and update aws.json to avoid using EC2 but a fargate solution when deploying

--- app/public/javascript/base.js
+++ app/public/javascript/base.js
@@ -84,4 +84,80 @@ $(document).ready(function () {
             });
         });
     });
+
+    if($(window).width() > 800) {
+        $(document).on("mouseover", ".news__item", function (_event, _element) {
+
+            var newsItem = document.querySelectorAll('.news__item');
+            newsItem.forEach(function (element, index) {
+                element.addEventListener('mouseover', function () {
+                    $('.item-bg').addClass('active');
+                    $('.news__item').removeClass('active');
+                });
+
+                element.addEventListener('mouseleave', function () {
+                    $('.item-bg').removeClass('active');
+                    $('.news__item').removeClass('active');
+                });
+
+            });
+
+        });
+    }
+
+    var swiper = new Swiper('.news-slider', {
+        effect: 'coverflow',
+        grabCursor: true,
+        loop: true,
+        centeredSlides: true,
+        keyboard: true,
+        spaceBetween: 0,
+        slidesPerView: 'auto',
+        speed: 300,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 0,
+            modifier: 3,
+            slideShadows: false
+        },
+        breakpoints: {
+            480: {
+                spaceBetween: 0,
+                centeredSlides: true
+            }
+        },
+        simulateTouch: true,
+        navigation: {
+            nextEl: '.news-slider-next',
+            prevEl: '.news-slider-prev'
+        },
+        pagination: {
+            el: '.news-slider__pagination',
+            clickable: true
+        },
+        on: {
+            init: function () {
+                $('.swiper-slide-active .news__item').addClass('active');
+                $('.item-bg').addClass('active');
+            }
+        }
+    });
+
+    swiper.on('touchEnd', function () {
+        $('.news__item').removeClass('active');
+        $('.swiper-slide-active .news__item').addClass('active');
+    });
+
+    swiper.on('slideChange', function () {
+        $('.news__item').removeClass('active');
+    });
+
+    swiper.on('slideChangeTransitionEnd', function () {
+        $('.news__item').removeClass('active');
+        $('.swiper-slide-active .news__item').addClass('active');
+
+        $('.item-bg').addClass('active');
+    });
 });
+
